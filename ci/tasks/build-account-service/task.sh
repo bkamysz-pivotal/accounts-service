dc2277fa4fc7081776681d3b3bdd84a5b19367bd
bkamysz-pivotal <bkamysz@pivotal.io>
2017-06-08 13:29:43 -0700

    Fix build script

--- ci/tasks/build-account-service/task.sh
+++ ci/tasks/build-account-service/task.sh
@@ -6,8 +6,8 @@ set -e
 
 pushd accounts-service
 #  ./gradlew -PversionNumber=$VERSION clean assemble
-  ./gradlew -PversionNumber=$VERSION clean assemble
+  ./gradlew clean assemble
 popd
 
 #cp project/build/libs/$ARTIFACT_ID-$VERSION.jar build-output/.
-cp project/build/libs/$ARTIFACT_ID.jar build-output/.
+cp accounts-service/build/libs/$ARTIFACT_ID.jar build-output/.
