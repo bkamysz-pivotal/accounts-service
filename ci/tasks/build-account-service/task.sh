70406c335eb044b5e276f83709d36e5aeb67c3be
bkamysz-pivotal <bkamysz@pivotal.io>
2017-06-08 18:29:48 -0700

    Add cf push with plugin

--- ci/tasks/build-account-service/task.sh
+++ ci/tasks/build-account-service/task.sh
@@ -4,16 +4,10 @@ set -e
 
 #VERSION=`cat version/number`
 
-pwd
-
 pushd accounts-service
 #  ./gradlew -PversionNumber=$VERSION clean assemble
   ./gradlew clean assemble
-
-  pwd
-
-  ls build/libs
 popd
 
 #cp project/build/libs/$ARTIFACT_ID-$VERSION.jar build-output/.
-#cp accounts-service/build/libs/$ARTIFACT_ID.jar accounts-service/.
+cp accounts-service/build/libs/$ARTIFACT_ID.jar build-output/.
